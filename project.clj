5f5121872d097e5f8d6b5a0d646cdde69a56a0d0
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-06-25 21:38:42 -0400

    Update noir dependency to 1.0.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "A fully functional blog that serves an example of a noir project."
             :dependencies [[org.clojure/clojure "1.2.1"]
                            [clj-time "0.3.0"]
-                           [noir "0.4.0"]
+                           [noir "1.0.0"]
                            [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                            [simpledb "0.1.4"]]
             :main noir-blog.server)
