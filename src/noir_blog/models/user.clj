b1e358e24af7a49dfda1299db09c4a10a969c487
zoka <ztomicic@gmail.com>
2012-03-12 05:08:44 +1100

    Updated dependencies. Fixed edit! and improved add!

--- src/noir_blog/models/user.clj
+++ src/noir_blog/models/user.clj
@@ -23,14 +23,17 @@
 (defn prepare [{password :password :as user}]
   (assoc user :password (crypt/encrypt password)))
 
-(defn valid? [{:keys [username password]}]
+(defn valid-user? [username]
   (vali/rule (not (db/get-in :users [username]))
              [:username "That username is already taken"])
   (vali/rule (vali/min-length? username 3)
              [:username "Username must be at least 3 characters."])
+  (not (vali/errors? :username :password)))
+
+(defn valid-psw? [password]
   (vali/rule (vali/min-length? password 5)
              [:password "Password must be at least 5 characters."])
-  (not (vali/errors? :username :password)))
+  (not (vali/errors? :password)))
 
 ;; Operations
 
@@ -46,14 +49,18 @@
         (session/put! :username username))
       (vali/set-error :username "Invalid username or password"))))
 
-(defn add! [{:keys [username password]}]
-  (let [user (-> {:username username :password password}
-               (prepare))]
-    (when (valid? user)
-      (store! user))))
+(defn add! [{:keys [username password] :as user}]
+  (when (valid-user? username)
+    (when (valid-psw? password)
+      (-> user (prepare) (store!)))))
 
-(defn edit! [user]
-  (add! user))
+(defn edit! [{:keys [username old-name password]}]
+  (let [user {:username username :password password}]
+    (println "edit user" username "o" old-name "p" password)
+    (if (= username old-name)
+      (when (valid-psw? password)
+        (-> user (prepare) (store!)))
+      (add! user))))
 
 (defn remove! [username]
   (db/update! :users dissoc username))
